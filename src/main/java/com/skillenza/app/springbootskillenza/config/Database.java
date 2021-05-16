b8cf917496bdb1f40e63d2c8959698e36f0afeff
Shantanu Shubham <shantanu0707@gmail.com>
2021-05-16 22:49:10 +0530

    [Update] DB Error Solved

--- src/main/java/com/skillenza/app/springbootskillenza/config/Database.java
+++ src/main/java/com/skillenza/app/springbootskillenza/config/Database.java
@@ -2,21 +2,26 @@ package com.skillenza.app.springbootskillenza.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class Database {
 
+    @Autowired
+    private Environment environment;
+
     public @Bean
     MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(environment.getProperty("DBClient"));
     }
 
     public @Bean
     MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "spring");
+        return new MongoTemplate(mongoClient(), environment.getProperty("DBName"));
     }
 
 }
